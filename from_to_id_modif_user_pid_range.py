eb0bd847dc0c83c35d103b0d85aaeaa3bf4b5e33
Admax <31325354+Admarax@users.noreply.github.com>
2017-08-25 16:40:14 +0200

    Fix of the page range + error handeling
    Fixed the page range method, made it working with just one number. Handeling fatal error where the picture wasn't there anymore so the programe crashed because it couldn't find it. Added some how to use text.

--- from_to_id_modif_user_pid_range.py
+++ from_to_id_modif_user_pid_range.py
@@ -12,6 +12,14 @@ else:
 # save dir
 os.chdir(".\Images")
 
+# Welcoming message
+print('''Hello and welcome to my Gelbooru image scrapper. First you will be required to enter how many pages you 
+want to go through. You can enter a range of page, for example I want page 5 to 7 I will type: "5 7".
+If I just want to start from the first one to another, first to tenth for example, I will type: "10".
+After that you will be asked how many images do you want per page, there is a hard limit of 100 per page.
+For the tags you can enter as many as you want, you have to separate them by a space.
+Enjoy !\n''')
+
 # getting user inputs
 user_pid_range = ''
 val_entry = False
@@ -19,19 +27,28 @@ while val_entry is False:
     user_pid_range = input("Page range (x  y): ")
     user_pid_range = user_pid_range.split(" ")
 
-    try:  # test if range is negative and if inputs are ints
-        if int(user_pid_range[0]) > int(user_pid_range[1]):
-            print("The provided range is negative.")
-        else:
+    try:  # test if range is negative, if inputs are ints, if inputs are 1 or 2 numbers
+
+        if len(user_pid_range) < 2:  # if the input is only one number
+            user_pid_range[0] = int(user_pid_range[0])  # test if int
             val_entry = True
             print("validity to true")
-            break
+            user_pid_range = list(range(0, int(user_pid_range[0])))  # generate a range of pages
+
+        elif len(user_pid_range) > 2:
+            print("You entered more than 2 numbers")
+
+        else:
+            if int(user_pid_range[0]) > int(user_pid_range[1]):  # are the input positive
+                print("The provided range is negative.")
+
+            else:
+                val_entry = True
+                print("validity to true")
+                user_pid_range = list(range(int(user_pid_range[0])-1, int(user_pid_range[1])))  # generate rng of pages
 
     except ValueError:
         print("Can't process input")
-
-# make list for the for loop to iterate through
-user_pid_range = list(range(int(user_pid_range[0])-1, int(user_pid_range[1])))
 
 print(user_pid_range)
 
@@ -46,15 +63,21 @@ while isinstance(user_limit, int) is False:
 
 user_tags = input("Enter the tags please: ") or "rating:safe"
 
+post_url = ''
 def post_retreive():
 
-    # find posts and download content
+    global post_url
+
     for urls in soup.find_all('post'):
         posts_list.append(urls)  # on ajoute les posts a une liste pour les référencer
         print(posts_list.index(urls) + 1)  # display the index +1
         print("- " + urls.get("file_url") + "\n")  # affiche les urls des images
-        post_url = request.urlretrieve("https:" + urls.get("file_url"), urls.get("id")
-                                       + "." + urls.get("file_url").split(".")[-1])  # dl and naming with respective ids
+        try:
+            post_url = request.urlretrieve("https:" + urls.get("file_url"), urls.get("id")
+                                           + "." + urls.get("file_url").split(".")[-1]) # dl and naming with respective ids
+        except request.HTTPError:
+            print("The post couldn't be retrieved")
+
     return post_url
 
 posts_list = []
